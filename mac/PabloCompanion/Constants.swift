88a3a43ae89dfd918c52b22fb246c6c194d85950
Kurt Niemi <kurtn@pablo.health>
2026-03-19 20:57:32 -0400

    feat: add auth callback URL scheme and redirect URI constants

--- mac/PabloCompanion/Constants.swift
+++ mac/PabloCompanion/Constants.swift
@@ -3,4 +3,6 @@ import Foundation
 enum AppConstants {
     static let appBundleID = Bundle.main.bundleIdentifier ?? "health.pablo.companion"
     static let keychainAccessGroup = "L8KG4FA2R9.\(appBundleID)"
+    static let callbackURLScheme = "pablohealth"
+    static let redirectURI = "\(callbackURLScheme)://callback"
 }
